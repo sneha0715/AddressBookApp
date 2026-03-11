510e297c2d2a6a684ed2c216fecb99f7c086565d
sneha0715 <157895540+sneha0715@users.noreply.github.com>
2026-03-11 23:19:07 +0530

    [Sneha Agrawal]Add . Address Book Setup

---
+++ src/test/java/com/sneha/Address/Book/AddressBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sneha.Address.Book;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AddressBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
